024978b77ceb907b3480f166b4886cf885179214
almaz.sh <almaz.sh@soft-artel.com>
2024-12-06 18:17:56 +0300

    bla

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React bla bla bla
+          Learn React bla bla bla bla
         </a>
       </header>
     </div>
